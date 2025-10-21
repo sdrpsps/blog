a440d25720a559e214d85e0fa3a5132f1335fbc4
sunny <sunny@bytespark.app>
2025-10-22 07:40:35 +0800

    Fix nextjs remote images url

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "static.bytespark.me",
+        hostname: "*.bytespark.me",
       },
     ],
   },
